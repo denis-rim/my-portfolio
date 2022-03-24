db71fdc5b8881835de1ad485965b3bdac9c20d0e
Denis Mazurchuk <denis.aurorim@gmail.com>
2022-03-24 12:35:27 +0200

    Skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,26 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  console.log(itemClass);
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close';
+  }
+
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open';
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
