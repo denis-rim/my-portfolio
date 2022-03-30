5ec38d031353572874891ec706ea01e62291983f
Denis Mazurchuk <denis.aurorim@gmail.com>
2022-03-30 13:06:21 +0300

    Portfolio section and swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,6 +105,18 @@ modalCloses.forEach((closeButton, index) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+const swiper = new Swiper('.porftolio__container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
